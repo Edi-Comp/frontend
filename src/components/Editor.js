cc03b03a48145b85cc33e195755ad87e00a5ec88
Zaid Bhimala <zaidm124@gmail.com>
2022-12-26 12:14:27 +0530

    Send and recieve cursor to socket from frontend

--- src/components/Editor.js
+++ src/components/Editor.js
@@ -1,17 +1,22 @@
 import React, { useState, useEffect, useRef, useCallback } from "react";
 import Quill from "quill";
-import { io } from "socket.io-client";
+import { io, Socket } from "socket.io-client";
 // editor styles
 import "quill/dist/quill.snow.css";
 function Editor() {
   const [socket, setSocket] = useState();
   const [quill, setQuill] = useState();
   const [online, setOnline] = useState([]);
+  const [user,setUser]=useState("");
   var timeout;
 
+  const server = "http://localhost:5000";
+  // const server="https://edicomp-backend.onrender.com/"
   useEffect(() => {
-    const s = io("https://edicomp-backend.onrender.com/");
+    const s = io(server);
     setSocket(s);
+    const name = window.prompt("Enter your name:");
+    setUser(name);
     return () => {
       saveDocument();
       s.disconnect();
@@ -30,6 +35,7 @@ function Editor() {
 
   //To Push code to the database
   const saveDocument = () => {
+    if(socket==null)return;
     console.log("saving from frontend");
     socket.emit("save-document", quill.getText());
   };
@@ -42,9 +48,12 @@ function Editor() {
       quill.enable();
     });
 
-    const name = window.prompt("Enter your name:");
-    socket.emit("join-room", 1, name);
+    socket.emit("join-room", 1, user);
   }, [socket, quill]);
+
+  useEffect(()=>{
+    console.log(user);
+  },[user])
 
   // Update doc for other users when one of them saves their document
   useEffect(() => {
@@ -58,15 +67,15 @@ function Editor() {
   // Save Document on new user connecting
   useEffect(() => {
     if (quill == null || socket == null) return;
-    socket.on("user-connected", (username,room) => {
+    socket.on("user-connected", (username, room) => {
       saveDocument();
       console.log(room);
-      setOnline(room)
+      setOnline(room);
     });
-    socket.on("user-disconnected", (username,room) => {
+    socket.on("user-disconnected", (username, room) => {
       console.log(username + " diconnected");
       console.log(room);
-      setOnline(room)
+      setOnline(room);
     });
   }, [socket, quill]);
 
@@ -84,12 +93,25 @@ function Editor() {
     };
   }, [socket, quill]);
 
+  // Send and get cursor index
+  useEffect(()=>{
+    if(quill==null || socket==null)return
+    quill.on("selection-change",()=>{
+      console.log(user);
+      socket.emit("send-cursor",user,quill.getSelection().index);
+    })
+
+    socket.on("get-cursor",(cursors)=>{
+      console.log(cursors);
+    })
+  },[socket,quill])
+
   // Send changes from socket to socket
   useEffect(() => {
     if (quill == null || socket == null) return;
     const handler = (delta, oldDelta, source) => {
       if (source !== "user") return;
-      socket.emit("send-message", delta);
+      socket.emit("send-message", delta,quill.getSelection().index);
     };
     quill.on("text-change", handler);
     return () => {
